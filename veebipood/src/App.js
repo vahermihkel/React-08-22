b306417eab39de58ae73d9d0698135158105ec11
mihkelvaher <mihkel.vaher@industry62.com>
2022-08-29 21:02:34 +0300

    React algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+// ctrl + ä on kommentaaride lisamiseks
+// (saan edit -> toggle line comment)
+// kommentaarid ei mõjuta koodi
+// alt="" <--- alternative text
+// 1. kui pildi aadress (src="") on vigane, siis näidatakse pildi asemel
+// alternative texti
+// 2. pimedad ei näe pilte, neile loetakse ette alt (kohustuslik riigiettevõtetes)
+// 3. arendajad (server ei tööta) näevad ka
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" alt="Nobe pilt auto" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" />
+      <button className="nupp">Vajuta mind</button>
+    </div>
+  );
+}
+
+export default App;
