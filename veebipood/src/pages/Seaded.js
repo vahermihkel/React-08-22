c84a2f440e9770de0b4e2be4b7819a7c1efec4a5
mihkelvaher <mihkel.vaher@industry62.com>
2022-09-14 20:43:48 +0300

    Firebase, defaultValue, funktsiooni argument/parameeter

--- veebipood/src/pages/Seaded.js
+++ veebipood/src/pages/Seaded.js
@@ -4,24 +4,27 @@ import { useState } from "react";
 // püstised kriipsud ||      <>|   < - lihtsalt vajutus     > - shift+<     | - alt gr+<
 
 function Seaded() {                   // getItem tähistab viimast setItem panekut
-  const [keel, muudaKeel] = useState(localStorage.getItem("veebilehe_keel") || "est");
+  const [keel, uuendaKeel] = useState(localStorage.getItem("veebilehe_keel") || "est");
   const telRef = useRef();
   const emailRef = useRef();
 
-  const muudaKeelEst = () => {
-    muudaKeel("est"); // muudab HTMLi
-    localStorage.setItem("veebilehe_keel", "est"); // salvestab
-  }
+  // const muudaKeelEst = () => {
+  //   uuendaKeel("est"); // muudab HTMLi
+  //   localStorage.setItem("veebilehe_keel", "est"); // salvestab
+  // }
 
-  const muudaKeelEng = () => {
-    muudaKeel("eng");
-    localStorage.setItem("veebilehe_keel", "eng");
-  }
+  // const muudaKeelEng = () => {
+  //   uuendaKeel("eng");
+  //   localStorage.setItem("veebilehe_keel", "eng");
+  // }
 
-  const muudaKeelRus = () => {
-    muudaKeel("rus");
-    localStorage.setItem("veebilehe_keel", "rus");
-  }
+  // const muudaKeelRus = () => {
+  //   uuendaKeel("rus");
+  //   localStorage.setItem("veebilehe_keel", "rus");
+  // }
+
+  // const abil loon uue muutuja (viite)
+  // localStorage <--- JavaScripti enda muutuja (viide)
 
   const sisestaTel = () => {
     localStorage.setItem("tel", telRef.current.value);
@@ -31,21 +34,26 @@ function Seaded() {                   // getItem tähistab viimast setItem panek
     localStorage.setItem("email", emailRef.current.value);
   }
 
+  const muudaKeel = (uusKeel) => {
+    uuendaKeel(uusKeel);
+    localStorage.setItem("veebilehe_keel", uusKeel);
+  }
+
   return ( 
     <div>
         <label>Meie telefoninumber </label>
-        <input ref={telRef} type="text" />
+        <input ref={telRef} defaultValue={localStorage.getItem("tel")} type="text" />
         <button onClick={sisestaTel}>Sisesta</button>
         <br />
         <label>Meie email </label>
-        <input ref={emailRef} type="text" />
+        <input ref={emailRef} defaultValue={localStorage.getItem("email")} type="text" />
         <button onClick={sisestaEmail}>Sisesta</button>
         <br />
 
 
-        <button onClick={muudaKeelEst}>est</button>
-        <button onClick={muudaKeelEng}>eng</button>
-        <button onClick={muudaKeelRus}>rus</button>
+        <button onClick={() => muudaKeel("est")}>est</button>
+        <button onClick={() => muudaKeel("eng")}>eng</button>
+        <button onClick={() => muudaKeel("rus")}>rus</button>
         { keel === "est" && <div>Leht on eesti keelne</div> }
         { keel === "eng" && <div>Leht on inglise keelne</div> }
         { keel === "rus" && <div>Leht on vene keelne</div> }
