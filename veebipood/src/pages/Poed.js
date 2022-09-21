24b14494e6f1259f80834bbf4f07d0a53c3da025
mihkelvaher <mihkel.vaher@industry62.com>
2022-09-21 22:06:22 +0300

    Arrays

--- veebipood/src/pages/Poed.js
+++ veebipood/src/pages/Poed.js
@@ -3,13 +3,17 @@
 // array - list - massiiv
 //     ["Mustamäe", "Kristiine", "Lasnamäe", "Põhja-Tallinn", "Kesklinn"] 
 
+import { useRef } from "react";
 import { useState } from "react";
+
+import poedFailist from "../poed.json";
 
 //      /poed   
 function Poed() {
   // muutuja, mille väärtuseks on array (väärtuste kogumik), iga element on eristatud komaga
-  //                                    0           1           2           3           4               5
-  const [poed, uuendaPoed] = useState(["Mustamäe", "Õismäe", "Kristiine", "Lasnamäe", "Põhja-Tallinn", "Kesklinn"]);
+  //                                    0           1           2           3           4          5
+  const [poed, uuendaPoed] = useState(poedFailist);
+  const poodRef = useRef();
 
   const sorteeri = () => {
     poed.sort(); // JavaScripti sissekirjutatud funktsionaalsus -> sorteeri A-Z
@@ -41,20 +45,88 @@ function Poed() {
     uuendaPoed(poed.slice());
   }
 
+  const lisaPood = () => {
+    poed.push(poodRef.current.value);    // animals.push("cows")     animals.push("chicken", "cats", "dogs")
+    uuendaPoed(poed.slice()); // .slice() -- teeb koopia EHK kustutab esialgse mälukoha
+  }
+  // function lisaPood() {
+
+  // }
+
+  const tyhjenda = () => {
+    uuendaPoed([]);
+  }
+
+  const filtreeri = () => {
+    // poed.     <---- manipuleerima / muutma   // filtreerime sisse kõik kellel on lõpus "-mäe"
+    const vastus = poed.filter(pood => pood.includes("mäe")); // filter noole järel pean ütlema tingimuse, mis juhul ta jäetakse alles
+    uuendaPoed(vastus);
+  }
+
+  const muudaK6iki = () => {
+    const vastus = poed.map(pood => pood.charAt(0) + pood); // .map noole järel pean ütlema mille ma panen igaühe asemele
+    uuendaPoed(vastus);
+  }
+
+  const indexRef = useRef();
+  const uusRef = useRef();
+
+  const muudaPood = () => {
+    // ["Mustamäe", "Õismäe", "Kristiine",  "Põhja-Tallinn", "Kesklinn"][1] = "Kakumäe";
+    poed[indexRef.current.value] = uusRef.current.value;
+    uuendaPoed(poed.slice());
+  }
+
+  const [valitudPood, uuendaValitudPood] = useState("");
+
+  const vaata = (pood) => {
+    console.log(pood); // logib meie brauseri konsooli (parem klõps -> inspect -> console)
+    uuendaValitudPood(pood);
+  }
+
+  // const vaata = (index) => {
+  //   //console.log(pood); // logib meie brauseri konsooli (parem klõps -> inspect -> console)
+  //   uuendaValitudPood(poed[index]);
+  // }
+
   return ( 
     <div>
       <button onClick={sorteeri}>Sorteeri A-Z</button>
+      <button onClick={tyhjenda}>Tühjenda</button> 
+      <button onClick={filtreeri}>Jäta vaid "mäe"-d sisaldavad alles</button>
+      <button onClick={muudaK6iki}>Muuda kõiki</button>
+      <br />
+
+      <label>Uus pood</label> <br />
+      <input ref={poodRef} type="text" /> <br />
+      <button onClick={lisaPood}>Lisa uus pood</button> <br />
+
       {/* .map() on array-de kuvamiseks HTMLs, siis teen selle funktsionaalsuse, mis on sisus tsükkel
       tehakse noole paremat poolt nii mitu korda kui mitu komaga eristatud elementi mul on */}
-      {poed.map((pood,index) => <div key={pood}>{pood} <button onClick={() => kustuta(index)}>x</button></div>)} 
+      <div>Sinu valitud pood: {valitudPood}</div>
+
+      <div>Poode on {poed.length} tk</div>
+
+      {poed.map((pood,index) => 
+        <div key={pood}>{pood} 
+          <button onClick={() => kustuta(index)}>x</button>
+          <button onClick={() => vaata(pood)}>Vaata</button>
+        </div>)} 
+
+      {/* <div *ngFor="let pood of poed; let i = index">{pood} <button onClick={() => kustuta(index)}>x</button></div> */}
+
+      <label>Järjekorranumber</label> <label>Uus väärtus</label> <br />
+      <input ref={indexRef} type="text" /> <input ref={uusRef} type="text" /> <br />
+      <button onClick={muudaPood}>Muuda pood</button> <br />
+
       {/* {idd.map((id,index) => <div key={id}>{id} <button onClick={() => kustuta(index)}>x</button></div>)}  */}
 
-      {/* <div>Mustamäe <button>x</button></div>
-      <div>Õismäe <button>x</button></div>
-      <div>Kristiine <button>x</button></div>
-      <div>Lasnamäe <button>x</button></div>
-      <div>Põhja-Tallinn <button>x</button></div>
-      <div>Kesklinn <button>x</button></div> */}
+      {/* <div key="Mustamäe">Mustamäe <button onClick={() => kustuta(0)}>x</button></div>
+      <div key="Õismäe">Õismäe <button onClick={() => kustuta(1)}>x</button></div>
+      <div key="Kristiine">Kristiine <button onClick={() => kustuta(2)}>x</button></div>
+      <div key="Lasnamäe">Lasnamäe <button onClick={() => kustuta(3)}>x</button></div>
+      <div key="Põhja-Tallinn">Põhja-Tallinn <button onClick={() => kustuta(4)}>x</button></div>
+      <div key="Kesklinn">Kesklinn <button onClick={() => kustuta(5)}>x</button></div> */}
     </div> );
 }
 
