c5c0f00144b201971ab23ab41e0826273e93dcd4
mihkelvaher <mihkel.vaher@industry62.com>
2022-10-17 19:02:08 +0300

    Update Cart.jsx

--- webshop/src/pages/Cart.jsx
+++ webshop/src/pages/Cart.jsx
@@ -3,13 +3,20 @@ import { useState } from "react";
 function Cart() {
   const [cart, setCart] = useState( JSON.parse(localStorage.getItem("cart")) || [] );
 
+  const remove = (productIndex) => {
+    cart.splice(productIndex,1);
+    setCart(cart.slice());
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
   return ( 
     <div>
-      {cart.map(element => 
+      {cart.map((element, index) => 
         <div>
           <img src={element.image} alt="" />
           <div>{element.name}</div>
           <div>{element.price}</div>
+          <button onClick={() => remove(index)}>x</button>
         </div>)}
     </div> );
 }
